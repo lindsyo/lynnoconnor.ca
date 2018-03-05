274266f01e218769d5f3ed04bad3880121f5536e
Lynn <lindsy.oc@gmail.com>
2018-03-05 20:50:49 +0800

    navigation and spacing

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -11,7 +11,48 @@ $(document).ready(function(){
 
   var typed = new Typed('#typed', {
     stringsElement: '#typed-strings',
-    typeSpeed: 50
+    typeSpeed: 50,
+    showCursor: true
   });
 
-});
+// this is the smooth scrolling
+
+  // Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+  // end smooth scrolling
+
+});    //end jquery 
